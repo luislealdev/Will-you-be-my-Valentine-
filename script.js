2deb308e7da3517aaf9526914598853f861769d5
Luis Leal <luisrrleal@gmail.com>
2025-02-13 18:35:32 -0600

    Working

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Segura?",
+    "Enserio segura?",
+    "Estás bien?",
+    "Mi amor ;;;(...",
+    "Solo piénsalo!",
+    "Si me dices que no, voy a llorar...",
+    "Ya voy a llorar...",
+    "Mira mi lagrimita ;(...",
+    "Bueno, tu te la pierdes...",
+    "No es ciertoooooo, ya dime que si! ❤️"
 ];
 
 let messageIndex = 0;
